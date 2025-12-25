d2c04a6ef7c02e15095079d4f5f8c333b096c569
unknown <rjspyk5@gmail.com>
2025-12-25 22:56:33 +0600

    feat:setup basic proxy for protected all routes

--- proxy.ts
+++ proxy.ts
@@ -1,12 +1,25 @@
+import { cookies } from 'next/headers'
 import { NextResponse, NextRequest } from 'next/server'
- 
-// This function can be marked `async` if using `await` inside
-export function proxy(request: NextRequest) {
-  return NextResponse.redirect(new URL('/home', request.url))
+
+export async function proxy(request: NextRequest) {
+    const cookie = await cookies();
+    const isLogin = cookie.get("access_token");
+    const currentPath = request.nextUrl.pathname;
+    // if not login then go to login page and keep the state of which page want to go
+    if ((currentPath !== "login" && !isLogin)) {
+        return NextResponse.redirect(new URL('/login', request.url))
+    }
+    // if login then go dashboard page and can't go to login page
+    if ((currentPath === "login" && isLogin)) {
+        return NextResponse.redirect(new URL('/dashboard', request.url))
+    }
+    // TODO:login check
+    // if login then check role and check he have access of the page or not
+    // then go desired page and also will implement double protection of the page with RoleGuardComponent and AuthGuard component and show dasboard menu based on their role
+    return NextResponse.redirect(new URL('/home', request.url))
 }
 
-
- 
 export const config = {
-  matcher: '/:path*',
+    matcher: '/:path*',
 }
+
