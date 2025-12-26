fde9589b5bfb5495442b4fac456c42d09c27f557
unknown <rjspyk5@gmail.com>
2025-12-26 23:28:11 +0600

    fix:middleware problem

--- proxy.ts
+++ proxy.ts
@@ -1,28 +1,27 @@
-import { cookies } from 'next/headers'
 import { NextResponse, NextRequest } from 'next/server'
 
 export async function proxy(request: NextRequest) {
-    const cookie = await cookies();
-    const isLogin = cookie.get("access_token");
+    const isLogin = request.cookies.get("access_token");
+    const token=isLogin?.value
 
-
-    
     const currentPath = request.nextUrl.pathname;
     // if not login then go to login page and keep the state of which page want to go
-    if ((currentPath !== "login" && !isLogin)) {
+    if ((currentPath !== "/login" && !isLogin)) {
         return NextResponse.redirect(new URL('/login', request.url))
     }
     // if login then go dashboard page and can't go to login page
-    if ((currentPath === "login" && isLogin)) {
+    if ((currentPath === "/login" && isLogin)) {
         return NextResponse.redirect(new URL('/dashboard', request.url))
     }
     // TODO:login check
     // if login then check role and check he have access of the page or not
     // then go desired page and also will implement double protection of the page with RoleGuardComponent and AuthGuard component and show dasboard menu based on their role
-    return NextResponse.redirect(new URL('/home', request.url))
+    return NextResponse.next()
 }
 
 export const config = {
-    matcher: '/:path*',
+  matcher: [
+    '/((?!_next|favicon.ico|images|assets|public|api/public).*)',
+  ],
 }
 
